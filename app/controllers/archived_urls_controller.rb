6768284a98420db6d10e40b67d0b6297a6d23b31
Philip Poots <philip.poots@gmail.com>
2016-11-24 20:29:20 +0100

    Keep filter around

--- app/controllers/archived_urls_controller.rb
+++ app/controllers/archived_urls_controller.rb
@@ -6,9 +6,9 @@ class ArchivedUrlsController < ApplicationController
     url.archive!
     if params[:detail]
       next_url = ExtractedUrl.canonical.active.order(created_at: :asc).limit(1)
-      redirect_to url_path(next_url)
+      redirect_to url_path(next_url, filter: params[:filter])
     else
-      redirect_to urls_path
+      redirect_to urls_path(filter: params[:filter])
     end
   end
 end
